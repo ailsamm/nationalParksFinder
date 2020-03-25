7f9ba4894ba4dba2f96a0dec13a26422a7839089
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-03-25 09:15:02 -0700

    Update input fields

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use-strict';
 
-const url = 'https://developer.nps.gov/api/v1/parks?stateCode=ca'
+const url = 'https://developer.nps.gov/api/v1/parks?stateCode='
 const apiKey = 'rOSgoEBaYhbpTlSU80U74n6e3eTdZDPAO6p39MlA';
 
 function processResults(states){
+    const searchUrl = url + states;
     const options = {
         headers: new Headers({
           "X-Api-Key": apiKey,
@@ -22,5 +23,20 @@ function watchForm(){
         processResults(states);
     });
 }
+/* $(function() {
+	$('.select').selectize({
+        delimiter: ',',
+        persist: false,
+        placeholder: "something",
+        maxItems: 5,
+        options: ["first", "second", "third"],
+        create: function(input) {
+            return {
+                value: input,
+                text: input
+            }
+        }
+    });
+}); */
 
 $(watchForm);
