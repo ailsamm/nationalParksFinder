ac5efccaab9c327be25ab4e2be829fa79a76e0c7
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-03-25 11:31:59 -0700

    Append state selections to endpoint URL

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 'use-strict';
 
-const url = 'http://crossorigin.me/https://developer.nps.gov/api/v1/parks?stateCode='
+const url = 'https://developer.nps.gov/api/v1/parks?stateCode='
 const apiKey = 'rOSgoEBaYhbpTlSU80U74n6e3eTdZDPAO6p39MlA';
 
 function processResults(states){
-    const searchUrl = url + states;
+    const stateString = formatStates(states);
+    const searchUrl = url + stateString;
+    console.log(searchUrl); 
 
     fetch(searchUrl, {
     method: 'GET',
     headers: {
         "X-Api-Key": apiKey,
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"}})
+        "Access-Control-Allow-Origin": "no-cors"}})
     .then(results => console.log(results));
 
 }   
 
+function formatStates(states) {
+    // create correct parameter format for passing to endpoint URL
+    let stateString = "";
+    for (let i=0; i < states.length; i++){
+        stateString += states[i] + ",";
+    }
+    return stateString.slice(0,-1)
+}
+
 function watchForm(){
-    $('form').submit(event => {
+    $("#submit-button").click(event => {
         event.preventDefault();
-        const states = $('#states').val();
-        $('#states').val("");
+        const states = $('#search').val();
+        $('#search').val("");
         processResults(states);
     });
 }
@@ -30,7 +41,8 @@ $(document).ready(function(){
         maxItems: 5,
         placeholder: "Enter state(s) here",
         delimiter: ',',
-        persist: false
+        persist: false,
+        items: ["abc", "bbb", "bbb"]
     });
 });
 
