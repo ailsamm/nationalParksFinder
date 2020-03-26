e74be315bde479f4900b7e07e428bf90c185b671
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-03-25 17:30:07 -0700

    Add functionality to remove selected options

--- index.js
+++ index.js
@@ -22,11 +22,19 @@ function processResults(states){
         })
         .then(responseJson => handleResults(responseJson.data))
         .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            $('.js-error-message').removeClass("hidden");
+            $('.js-error-message').text(`Something went wrong: ${err.message}. Please try again.`);
         });
     }   
 
 function handleResults(parks) {
+    $(".search-results").empty();
+
+    if (parks.length === 0){
+        $('.js-error-message').removeClass("hidden");
+        $('.js-error-message').text(`No results found. Please try again.`);
+    }
+
     for (let i = 0; i < parks.length; i++){
         console.log(parks[i]);
         $(".search-results").append(createResultHtml(parks[i]));
@@ -35,9 +43,11 @@ function handleResults(parks) {
 
 function createResultHtml(park) {
     return `<div class="result">\
-    <h3><a class="park-link" href="${park.url}" target="_blank">${park.fullName}, ${park.states}</a></h3>\
-    <p class="park-description"> ${park.description}</p>
-    <a class="park-email" href="mailto:${park.contacts.emailAddresses[0].emailAddress}">EMAIL</a><a class="park-phone" href="tel:${park.contacts.phoneNumbers[0].phoneNumber}">CALL</a>
+    <h3><a class="park-link" href="${park.url}" target="_blank">${park.fullName}</a></h3>\
+    <h4 class="park-state"><span class="park-state-title">STATE(S): </span>${park.states}</h4>\
+    <p class="park-description"> ${park.description}</p>\
+    <a class="park-email" href="mailto:${park.contacts.emailAddresses[0].emailAddress}"><i class="icon fas fa-at"></i>EMAIL</a>\
+    <a class="park-phone" href="tel:${park.contacts.phoneNumbers[0].phoneNumber}"><i class="icon fas fa-phone"></i>CALL</a>\
 </div>`;
 }
 
@@ -56,6 +66,7 @@ function formatParams(states, limit=10) {
 function watchForm(){
     $("#submit-button").click(event => {
         event.preventDefault();
+        $('.js-error-message').addClass("hidden");
         const states = $('#search').val();
         $('#search').val("");
         processResults(states);
@@ -68,8 +79,22 @@ $(document).ready(function(){
         placeholder: "Enter state(s) here",
         delimiter: ',',
         persist: false,
-        items: ["abc", "bbb", "bbb"]
     });
+    
+    $('#max-results').selectize({
+        create: true,
+        placeholder: "Max. results"
+    });
+
+    $(".items").on("click", ".item", function() {
+        // remove selected item from selected if clicks
+        let select = $('#search').selectize();
+        let selectSizeControl = select[0].selectize;
+        let selectedValue = $(this).attr("data-value");
+        select[0].selectize.removeItem(selectedValue);
+        select[0].selectize.refreshItems();
+        select[0].selectize.refreshOptions();
+        })
 });
 
 
